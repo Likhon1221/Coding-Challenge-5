2f24a8ccff581dbeb61192d8f85e1f793195a9a6
Likhon1221 <siddiqur.rahman1221@gmail.com>
2024-09-21 16:56:02 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -11,4 +11,35 @@ console.log(inventory);
 
 // Task 2. Create an Orders Array of Order Objects
 
-let orders = []
+let orders = [];
+
+// Task 3. Create a Function to Place an Order
+
+function placeOrder(customerName, orderedItems) {
+    let in_stock = true;
+    for (let i = 0; i < orderedItems.length; i++) {
+        const item = orderedItems[i];
+        for (let j = 0; j < inventory.length; j++) {
+            if (item.name == inventory[j].name) {
+                if (item.quantity > inventory[j].quantity) {
+                    in_stock = false;
+                    console.log(`${item.name} - out of stock`);
+                }
+                break;
+            }
+        }
+    }
+
+    if (in_stock) {
+        for (let i = 0; i < orderedItems.length; i++) {
+            const item = orderedItems[i];
+            for (let j = 0; j < inventory.length; j++) {
+                if (item.name == inventory[j].name) {
+                    inventory[j].quantity -= item.quantity;
+                    break;
+                }
+            }
+        }
+    }
+    orders.push({customerName: customerName, items: orderedItems, status: "Pending"});
+}
